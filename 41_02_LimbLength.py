37a35ef820c0010672c31eb9dde28f5dfce91d26
Monica2202 <82723251+Monica2202@users.noreply.github.com>
2024-07-02 22:16:52 +0700

    Update 41_02_LimbLength.py

--- 41_02_LimbLength.py
+++ 41_02_LimbLength.py
@@ -16,10 +16,10 @@ Code Challenge: Solve the Limb Length Problem.
 Sample Input:
 4
 1
-0	13	21	22
-13	0	12	13
-21	12	0	13
-22	13	13	0
+0	20	9	11
+20	0	17	11
+9	17	0	8
+11	11	8	0
 Sample Output:
 2
 '''
